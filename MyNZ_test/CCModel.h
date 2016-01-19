f8ad6244dd8ec17a0d0dbef3e51fa6ecf3dc8093
Chad <chad19921988@icloud.com>
2016-01-19 08:30:53 +0800

    添加模型

---
+++ MyNZ_test/CCModel.h
@@ -0,0 +1,15 @@
+//
+//  CCModel.h
+//  MyNZ_test
+//
+//  Created by 陈嘉健 on 16/1/19.
+//  Copyright © 2016年 Chad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCModel : NSObject
+@property (nonatomic,copy) NSString *name;
+@property (nonatomic,assign) NSInteger age;
+
+@end
---
+++ MyNZ_test/CCModel.m
@@ -0,0 +1,15 @@
+//
+//  CCModel.m
+//  MyNZ_test
+//
+//  Created by 陈嘉健 on 16/1/19.
+//  Copyright © 2016年 Chad. All rights reserved.
+//
+
+#import "CCModel.h"
+
+@implementation CCModel
+
+
+
+@end
