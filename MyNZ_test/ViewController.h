e9d72bb926dea0737a98985ee2d2c822e4ef881d
Chad <chad19921988@icloud.com>
2016-01-19 08:24:44 +0800

    Initial Commit

---
+++ MyNZ_test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyNZ_test
+//
+//  Created by 陈嘉健 on 16/1/19.
+//  Copyright © 2016年 Chad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyNZ_testTests/MyNZ_testTests.m
@@ -0,0 +1,39 @@
+//
+//  MyNZ_testTests.m
+//  MyNZ_testTests
+//
+//  Created by 陈嘉健 on 16/1/19.
+//  Copyright © 2016年 Chad. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MyNZ_testTests : XCTestCase
+
+@end
+
+@implementation MyNZ_testTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
