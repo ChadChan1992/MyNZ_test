0181476414169ef55aadf6510ab792d8724e0502
Chad <chad19921988@icloud.com>
2016-01-26 18:51:49 +0800

    功能开发1

---
+++ MyNZ_test/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  MyNZ_test
+//
+//  Created by 陈嘉健 on 16/1/26.
+//  Copyright © 2016年 Chad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ MyNZ_test/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  MyNZ_test
+//
+//  Created by 陈嘉健 on 16/1/26.
+//  Copyright © 2016年 Chad. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
