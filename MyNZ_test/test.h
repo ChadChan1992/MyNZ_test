941af7c34467b5740d46fcbf443b1660b26e5573
Chad <chad19921988@icloud.com>
2016-01-26 19:00:36 +0800

    添加name

--- MyNZ_test/test.h
+++ MyNZ_test/test.h
@@ -6,8 +6,9 @@
 //  Copyright © 2016年 Chad. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface test : NSObject
+@property (nonatomic,copy) NSString *name;
 
 @end
